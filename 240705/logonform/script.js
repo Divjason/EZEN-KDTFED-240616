f4fea12b05d5a944257f6d70217587d231c8c98a
Divjason <sejinpark@tcocs.com>
2024-07-08 09:01:33 +0900

    240708

--- 240705/logonform/script.js
+++ 240705/logonform/script.js
@@ -1,22 +1,23 @@
 const changePhone1 = () => {
-  const phone1 = document.getElementById("phone1").value;
+  const phone1 = document.querySelector("#phone1").value;
   if (phone1.length === 3) {
-    document.getElementById("phone2").focus();
+    document.querySelector("#phone2").focus();
   }
 };
 
 const changePhone2 = () => {
-  const phone2 = document.getElementById("phone2").value;
+  const phone2 = document.querySelector("#phone2").value;
   if (phone2.length === 4) {
-    document.getElementById("phone3").focus();
+    document.querySelector("#phone3").focus();
   }
 };
 
 const changePhone3 = () => {
-  const phone1 = document.getElementById("phone1").value;
-  const phone2 = document.getElementById("phone2").value;
-  const phone3 = document.getElementById("phone3").value;
+  const phone1 = document.querySelector("#phone1").value;
+  const phone2 = document.querySelector("#phone2").value;
+  const phone3 = document.querySelector("#phone3").value;
+
   if (phone1.length === 3 && phone2.length === 4 && phone3.length === 4) {
-    document.getElementById("token__button").removeAttribute("disabled");
+    document.querySelector("#token_button").removeAttribute("disabled");
   }
 };
