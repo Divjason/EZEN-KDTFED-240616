dca8e2f91a94c0f171658a9397f522c92c2ba48d
Divjason <sejinpark@tcocs.com>
2024-07-16 17:07:21 +0900

    240716

--- 240715/02flex/script.js
+++ 240715/02flex/script.js
@@ -1,3 +1,4 @@
+// Header Nav
 window.addEventListener("scroll", () => {
   let scroll = window.scrollY;
   const header = document.querySelector("header");
@@ -7,4 +8,41 @@ window.addEventListener("scroll", () => {
   } else {
     header.classList.remove("active");
   }
+});
+
+// Slick Slider
+$(".myslider").slick({
+  dots: false,
+  infinite: true,
+  autoplay: true,
+  speed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+  ],
 });
