9c6d904b142306317b239f03788677c6406b7e15
Divjason <sejinpark@tcocs.com>
2024-07-29 17:38:18 +0900

    240729

--- 240723/script.js
+++ 240723/script.js
@@ -1,3 +1,36 @@
+// Header
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  let scrollY = window.scrollY;
+  if (scrollY > 50) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
+
+// Trigger
+const trigger = document.querySelector(".trigger");
+trigger.addEventListener("click", function () {
+  const gnb = document.querySelector(".gnb");
+  const menus = document.querySelectorAll(".menu a");
+
+  this.classList.toggle("active");
+  gnb.classList.toggle("active");
+
+  menus.forEach((menu) => {
+    menu.addEventListener("click", () => {
+      trigger.classList.remove("active");
+      gnb.classList.remove("active");
+    });
+  });
+});
+
+// ScrollTo
+$(".menu a").click(function () {
+  $.scrollTo(this.hash || 0, 900);
+});
+
 // Slick Slider : History
 $(".history-slider").slick({
   dots: true,
@@ -5,6 +38,31 @@ $(".history-slider").slick({
   speed: 300,
   slidesToShow: 4,
   slidesToScroll: 2,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 });
 
 // Slick Slider : Project
---
+++ 240729/01js/script.js
@@ -0,0 +1,59 @@
+// const test1 = "mon";
+// test1 = "tue";
+
+// 프로그램을 개발
+// - width 1920px
+// - 슬라이드 개수 5개 고정
+
+// 스크롤을 내리게 되었을 때, 내려간 값에 따라서 무언가를 변경!!
+
+// let test2 = "mon";
+
+// test2 = "tue";
+
+// 조별 프로젝트
+
+// A
+// - 카테고리 안에 상품개수 10개
+// var category = 10;
+
+// B;
+// 장바구니에 담긴 상품개수
+// var category = 8;
+
+// 병합
+
+// var x = 10;
+
+// var z = x + y;
+
+// var y = 20;
+// console.log(z);
+
+// Not a Number => 자료의 형태 중 하나!!
+
+// const num1 = 1;
+// const num2 = 2;
+
+// const sum = num1 + num2;
+
+// console.log(typeof sum);
+
+// const str1 = "1";
+// const str2 = "2";
+
+// console.log(typeof str1);
+
+const num = 1000;
+const str = "원";
+
+const counter = num + str;
+console.log(typeof counter);
+
+const num1 = 1000;
+const str1 = "2000";
+
+const counter1 = num1 + str1;
+console.log(counter1);
+
+prompt("얼마를 원하세요?");
